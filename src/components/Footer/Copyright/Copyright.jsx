6801daee132eea6fb6420495a5d04c20a7bf99f2
Dishajain89 <dishajain08dj@gmail.com>
2026-07-09 21:17:46 +0530

    minor fixe

--- src/components/Footer/Copyright/Copyright.jsx
+++ src/components/Footer/Copyright/Copyright.jsx
@@ -6,7 +6,7 @@ const Copyright = () => {
     <section className={styles.copyright}>
       <div className={styles.container}>
         <p className={styles.text}>
-          © {new Date().getFullYear()} Kalpavaastu Design Studio INDORE. All Rights Reserved.<span className={styles.designer}>
+          © {new Date().getFullYear()} Kalpavaastu Design Studio Indore. All Rights Reserved.<span className={styles.designer}>
          Developed by <Link href="https://www.instagram.com/dizzytech_byd/?hl=en" target="_blank" rel="noopener noreferrer">Dizzytech_byd</Link>
         </span>
         </p>
