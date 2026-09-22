05dd0edcd4597af4b3412342638985f263e14516
Dishajain89 <dishajain08dj@gmail.com>
2026-09-22 16:40:34 +0530

    some responsive fixes bleyodblue print also

--- src/components/BeyondBlueprintHero/BeyondBlueprintHero.jsx
+++ src/components/BeyondBlueprintHero/BeyondBlueprintHero.jsx
@@ -6,27 +6,31 @@ import styles from "./BeyondBlueprintHero.module.scss";
 export default function BeyondBlueprintHero() {
   return (
     <section className={styles.hero}>
-      {/* Desktop Image */}
-      <Image
-        src="/images/blueprintHero.png"
-        alt="Beyond Blueprint - A Canvas of Non-Executions"
-        fill
-        priority
-        quality={100}
-        className={`${styles.heroImage} ${styles.desktopImage}`}
-        sizes="(min-width: 769px) 100vw, 1px"
-      />
+      {/* 1. Desktop & Laptop View (MacBook, Ultra-wides, 1080p, 1440p) */}
+      <div className={styles.desktopWrapper}>
+        <Image
+          src="/images/blueprintHero.png"
+          alt="Beyond Blueprint - A Canvas of Non-Executions"
+          fill
+          priority
+          quality={100}
+          className={`${styles.heroImage} ${styles.desktopImage}`}
+          sizes="(min-width: 1025px) 100vw, 1px"
+        />
+      </div>
 
-      {/* Mobile Image */}
-      <Image
-        src="/images/blueprintHeroMobile.png"
-        alt="Beyond Blueprint - A Canvas of Non-Executions Mobile"
-        fill
-        priority
-        quality={100}
-        className={`${styles.heroImage} ${styles.mobileImage}`}
-        sizes="(max-width: 768px) 100vw, 1px"
-      />
+      {/* 2. Tablet & Mobile View (iPad, iPhone, Androids) */}
+      <div className={styles.mobileWrapper}>
+        <Image
+          src="/images/blueprintHeroMobile.png"
+          alt="Beyond Blueprint - A Canvas of Non-Executions Mobile"
+          fill
+          priority
+          quality={100}
+          className={`${styles.heroImage} ${styles.mobileImage}`}
+          sizes="(max-width: 1024px) 100vw, 1px"
+        />
+      </div>
     </section>
   );
 }
